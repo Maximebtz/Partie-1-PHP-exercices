77921b66169c0da65badf7238d7c24dc4ca7fb9e
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-03-24 13:43:45 +0100

    Update exo12.php

--- exo12.php
+++ exo12.php
@@ -1,19 +1,48 @@
 <?php
-<?php
-$favcolor = "";
 
-switch ($favcolor) {
-  case "red":
-    echo "Your favorite color is red!";
+
+
+// $language = array(
+//   "FR" => "Salut",
+//   "EN" => "Hello",
+//   "ES" => "Hola",
+// );
+
+$users = array(
+  "Mickael"=>"FR",
+  "Virgile"=>"ES",
+  "Marie-Claire"=>"EN",
+);
+
+
+
+function direBonjour($langue, $pikachu){
+  switch($langue){ //if
+    case "FR" : echo "Salut ".$pikachu."<br>";
     break;
-  case "blue":
-    echo "Your favorite color is blue!";
+    case "ES" : echo "Hola ".$pikachu."<br>";
     break;
-  case "green":
-    echo "Your favorite color is green!";
-    break;
-  default:
-    echo "Your favorite color is neither red, blue, nor green!";
+    case "EN" : echo "Hello ".$pikachu."<br>"; //elseif
+    break; //else
+    default : echo "La langue ....";
+  }
 }
-?>
+
+// function showMessageFromName($name, $usertable, $translation) {
+//   $text = $translation[$usertable[$name]]." ".$name." !";
+//   echo $text;
+// }
+foreach($users as $key => $value){
+  direBonjour($value, $key);
+  // echo $value.$key;
+  // if($value == "FR"){
+  //   echo "Salut".$key."<br>";
+  // }
+  // elseif($value == "RU"){
+  //   echo "Privet".$key."<br>"; 
+  // }
+}
+
+// showMessageFromName("Virgile", $user, $language)
+
 ?>
