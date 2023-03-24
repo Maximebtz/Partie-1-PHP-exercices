19bf72d6abaab317eb6e74b671c6fbb9facdc4d0
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-03-24 13:49:24 +0100

    Update exo13.php

--- exo13.php
+++ exo13.php
@@ -1,12 +1,21 @@
 <?php
 
+//Marquer les notes dans une variable
 $notes = array(10,12,8,19,3,16,11,13,9);
 
+//Créer une variable qui compte combien il y a de notes
 $nbNote = count($notes);
-$resMoy = array_sum($notes);
-$moy = $resMoy / $nbNote;
-echo "sa moyenne générale est donc de : ";
-echo number_format($moy,2);
 
+//Créer une variable qui fait la somme des notes
+$resMoy = array_sum($notes);
+
+//Créer une variable qui calcul la moyenne
+$moy = $resMoy / $nbNote;
+
+//Afficher la phrase
+echo "sa moyenne générale est donc de : ";
+
+//Afficher la moyenne avec maximum 2 chiffres après la virgule avec "number_fotmat"
+echo number_format($moy,2);
 
 ?>
